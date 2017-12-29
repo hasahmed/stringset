79c2889468c70c2f3fa088e5df1294a6a829fef9
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-28 21:16:33 -0500

    automated commit for testing

--- servertest_helper.sh
+++ servertest_helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 server=silo.cs.indiana.edu
-git add -A :/ 2> dev/null
+git add -A :/ 2> /dev/null
 git commit -m "automated commit for testing"  2> /dev/null
 git push origin master 2> /dev/null
 
