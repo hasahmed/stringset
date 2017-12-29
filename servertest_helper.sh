0da03149867f952e8edcffacb57692225434310b
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-28 21:10:35 -0500

    automated commit for testing

--- servertest_helper.sh
+++ servertest_helper.sh
@@ -5,11 +5,10 @@ git add -A :/
 git commit -m "automated commit for testing" # 2> /dev/null
 git push origin master #2> /dev/null
 
-ssh hasahmed@$server << ENDHERE
-    cd stringset
-    git pull origin master #2> /dev/null
-    make grind 2> grind.log
-    make clean
-    cat grind.log
-    exit
-ENDHERE
+ssh hasahmed@$server
+cd stringset
+git pull origin master #2> /dev/null
+make grind 2> grind.log
+make clean
+cat grind.log
+exit
