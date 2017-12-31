7886e8a1e1e7b012104f48a59dc17778f69b51fd
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-31 01:47:47 -0500

    automated commit for testing

--- stringset.c
+++ stringset.c
@@ -32,9 +32,9 @@ void stringset_free(stringset *str_set){
 
 int stringset_add(stringset *str_set, const char *str){
     unsigned long hashcode = hash_code(str) % str_set->node_array_length;
-    printf("%lu\n", hashcode);
-    return hashcode;
-    node *tmp = str_set->node_array[0];
+//    printf("%lu\n", hashcode);
+//    return hashcode;
+    node *tmp = str_set->node_array[hashcode];
     while(tmp->next != NULL){
         if (strcmp(tmp->string, str) == 0)
             return 0;
