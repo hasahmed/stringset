17c1188bdee7240bf3288513dc2d560d874286c4
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-01-01 05:05:47 -0500

    automated commit for testing

--- main.c
+++ main.c
@@ -17,7 +17,8 @@ int main() {
 //    list_insert_new_last(s->node_array[0], "tacos");
 //    list_insert_new_last(s->node_array[1], "tacos");
     stringset_add(s, "I love my wife Katherine Inez Ahmed");
-    stringset_add(s, "I love my wife Katherine Inez Ahmed");
+    i = stringset_add(s, "I love my wife Katherine Inez Ahmed");
+    printf("%d\n", i);
     stringset_free(s);
 //    stringset *s = stringset_new(10);
 //    node *node_list = list_init();
