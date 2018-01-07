d2859458e8a5395d46ca486b934eda93adec961e
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-01-06 20:25:42 -0500

    automated commit for testing

--- stringset.c
+++ stringset.c
@@ -150,8 +150,10 @@ void stringset_rehash(stringset *str_set) {
             }
         } //else array entry i IS null so ignore it
     }
-    free(new_str_set->node_array);
-    free(new_str_set);
+    free(str_set->node_array);
+    str_set->node_array = new_str_set->node_array;
+//    free(new_str_set->node_array);
+//    free(new_str_set);
 //    free(str_set->node_array);
     // we've gotten to the end so now all that is left to do is reassign pointers
 //    str_set->node_array_length = new_str_set->node_array_length;
