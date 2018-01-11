9d8a73c411325a6983b7a919b5d2a7d3c23cc502
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-01-11 14:49:36 -0500

    automated commit for testing

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main() {
     stringset_add(s, "air heads");
     stringset_add(s, "ir heads");
     stringset_add(s, "r heads");
-    stringset_add(s, " heads");
+//    stringset_add(s, " heads");
 //    stringset_add(s, " heads");
     int i = stringset_add(s, " heads");
     printf("result: %d\n", i);
