9a41fdf3df9a9d138a62913af92ce4b98906c0b7
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-01-13 20:09:01 -0500

    changed tests

--- tests/tests.c
+++ tests/tests.c
@@ -6,18 +6,20 @@
 #include "../stringset.h"
 #include "../util.h"
 
-node *next, *first, *n;
+node *next, *first, *n, *nodelist;
 
 int node_test_suite_init(){
     next = node_new("goodbye", NULL); //last in list
     first = node_new("hello", next); //first in list
     n = node_new("hello", NULL);
+    nodelist = node_new("list", NULL);
     return 0;
 }
 int node_test_suite_cleanup(){
     node_free(next);
     node_free(first);
     node_free(n);
+    node_free_list(nodelist);
     return 0;
 }
 
@@ -32,14 +34,28 @@ void test_linked_nodes(){
     CU_ASSERT_PTR_NULL(next->next);
 }
 
+void test_node_list_insert_after(){
+    node *tmp = list_insert_new_after(nodelist, "tacos");
+    CU_ASSERT_EQUAL(tmp, nodelist->next);
+    CU_ASSERT_PTR_NULL(nodelist->next->next);
+}
+
 int main(){
     CU_initialize_registry(); //must call first
+
+    //node tests
     CU_pSuite node_suite = CU_add_suite("Node Tests", &node_test_suite_init, &node_test_suite_cleanup);
     if(node_suite == 0){
         puts("successfully added suite");
     }
     CU_ADD_TEST(node_suite, test_node_new__node_free);
     CU_ADD_TEST(node_suite, test_linked_nodes);
+    CU_ADD_TEST(node_suite, test_node_list_insert_after);
     CU_basic_run_suite(node_suite);
+    //end node tests
+
+
+
+
     CU_cleanup_registry();
 }
